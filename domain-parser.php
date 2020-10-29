e46171ea564ee9c6ee129c2090411b999abc2c76
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2020-10-29 16:58:58 +0100

    init proof of concept

---
+++ domain-parser.php
@@ -0,0 +1,33 @@
+<?php
+
+$domains = file('http://data.iana.org/TLD/tlds-alpha-by-domain.txt', FILE_IGNORE_NEW_LINES + FILE_SKIP_EMPTY_LINES);
+
+array_shift($domains); // remove first row (version info)
+
+$result = [];
+$punycodes = [];
+foreach ($domains as $domain) {
+    $domainLowercase = mb_strtolower($domain);
+    $start = mb_substr($domainLowercase, 0, 4);
+
+    if ($start === 'xn--') {
+        $punycodes[] = $domainLowercase;
+    } else {
+        $result[] = $domainLowercase;
+    }
+}
+
+foreach ($punycodes as $punycode) {
+    $result[] = idn_to_utf8($punycode);
+}
+
+$result = array_flip($result);
+$result = array_map(function ($value) {
+    return true;
+}, $result);
+
+ksort($result, SORT_NATURAL);
+
+echo 'Domains count: ' . count($result) . "\n";
+var_export($result);
+echo "\n";
