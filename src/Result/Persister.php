b3ec376e79b89898120e862fd5306311aabe1098
Volodymyr Stelmakh <volod.stelmakh@gmail.com>
2024-09-26 19:55:31 +0200

    add symfony filesystem component

--- src/Result/Persister.php
+++ src/Result/Persister.php
@@ -2,39 +2,39 @@
 
 namespace VStelmakh\UrlHighlight\DomainUpdater\Result;
 
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
 use VStelmakh\UrlHighlight\DomainUpdater\DomainList;
 
 class Persister
 {
     public static function create(): self
     {
-        return new self(new Formatter());
+        return new self(new Formatter(), new Filesystem());
     }
 
     public function __construct(
         private readonly Formatter $formatter,
+        private readonly Filesystem $filesystem,
     ) {}
 
     public function save(DomainList $domainList, string $resultPath, bool $isOverwrite): void
     {
-        $this->validateWritable($resultPath);
-        $this->validateOverwrite($resultPath, $isOverwrite);
+        $absolutePath = Path::makeAbsolute($resultPath, getcwd());
+        $this->validateOverwrite($absolutePath, $isOverwrite);
 
         $result = $this->formatter->format($domainList);
-        file_put_contents($resultPath, $result);
-    }
 
-    private function validateWritable(string $path): void
-    {
-        $dirName = dirname($path);
-        if (!is_writable($dirName)) {
-            throw new \RuntimeException(sprintf('Directory "%s" is not writable.', $dirName));
+        try {
+            $this->filesystem->dumpFile($absolutePath, $result);
+        } catch (\Throwable $e) {
+            throw new \RuntimeException(sprintf('Unable to save result. %s', $e->getMessage()), $e);
         }
     }
 
     private function validateOverwrite(string $path, bool $isOverwrite): void
     {
-        if (!$isOverwrite && file_exists($path)) {
+        if (!$isOverwrite && $this->filesystem->exists($path)) {
             throw new \RuntimeException(sprintf(
                 'File "%s" already exists. Consider setting "%s" parameter to true to overwrite existing file.',
                 $path,
